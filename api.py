0ab4ee2d7a5df7c0b0fc73efaa3c091914c9b3e3
CDarling25 <135234079+CDarling25@users.noreply.github.com>
2023-12-04 20:00:46 -0500

    started class, created init method

--- api.py
+++ api.py
@@ -4,7 +4,24 @@ from requests import get
 import json
 from ip2geotools.databases.noncommercial import DbIpCity
 
+class BLS():
+  def __init__(self, interest):
+      self.interest = interest
+      self.interest_Series = None
+      self.ip = None
+      self.location = None
+    
+  def set_interest_Series(self):
+    codes = pd.read_csv("cpi_item_codes.csv")
+    print(codes.head())
+    # for i in range(len(codes)):
+    #   if 
+
+
+
 def main():
+    test = BLS("food")
+    test.set_interest_Series()
     payload = {"seriesid": ["CUUR0000SA0", "SUUR0000SA0"],
             "startyear": "2013",
             "endyear": "2014",
